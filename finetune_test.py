f8d84b4a616e58faaf7c4fbd5ac4b8f0668cd562
ramrow <peij.xu0@gmail.com>
2025-05-15 15:53:50 -0700

    fixing error attempt

--- finetune_test.py
+++ finetune_test.py
@@ -37,7 +37,7 @@ tokenizer.pad_token = tokenizer.eos_token
 tokenizer.padding_side = "right"
 
 tokenized_dataset = dataset.map(tokenize, batched=True, remove_columns=dataset.column_names)
-print(tokenized_dataset['attention_mask'][0])
+print(tokenized_dataset['labels'])
 # tokenized_dataset = tokenized_dataset.rename_column("input_ids", "labels")
 # tokenized_dataset.set_format(type="torch", columns=["labels", "attention_mask"])
 
