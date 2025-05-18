3f6832bf90b39c2ba39a0ff17f561d47e84839b8
ramrow <peij.xu0@gmail.com>
2025-05-18 13:40:42 -0700

    results

--- test_results.py
+++ test_results.py
@@ -1,5 +1,16 @@
-from tensorboard import notebook
-log_dir = "results/runs"
-notebook.start("--logdir {} --port 4000".format(log_dir))
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    TrainingArguments,
+    DataCollatorForLanguageModeling,
+    Trainer,
+    pipeline,
+    logging,
+)
+from transformers import pipeline
 
+model_name_or_path = "llama-7b-python-finance" #path/to/your/model/or/name/on/hub
+pipe = pipeline("text-generation", model=model_name_or_path)
+print(pipe("### Instruction: What is the sentiment of this tweet? Please choose an answer from {negative/neutral/positive} ### Human: Bananas was founded in 1930s and have continued to exist today gaining a revenue of 130%")[0]["generated_text"])
 
