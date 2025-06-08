617baf3a7321c7b97cb84956b6d7ed81fce26807
ramrow <peij.xu0@gmail.com>
2025-06-08 16:09:25 -0700

    pipelining

--- model_test.py
+++ model_test.py
@@ -1,11 +1,11 @@
 from transformers import pipeline
 
 model_name_or_path = "llama-7b-foam" #path/to/your/model/or/name/on/hub
-pipe = pipeline("text-generation", model=model_name_or_path)
+pipe = pipeline("text-generation", model=model_name_or_path,)
 
 print(pipe("Question: Conduct a RAS simulation for the turbulent flow over " \
 "a backward-facing step using the pimpleFoam solver. Set the inlet velocity to" \
 " 10 m/s and the outlet pressure to 0. The walls should be no-slip, and the front "
 "and back should be empty. Use the k-epsilon turbulence model for this simulation. " \
 "The fluid is Newtonian with a kinematic viscosity of 1e-5 m²/s. The control settings " \
-"are: endTime = 0.40 and writeInterval = 0.005.")[0]["generated_text"])
+"are: endTime = 0.40 and writeInterval = 0.005."))
