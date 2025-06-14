915c8dfacf8d9c0ce118daa4f34d733b5f2c82c3
ramrow <peij.xu0@gmail.com>
2025-06-13 20:14:07 -0700

    added batch size

--- model_test.py
+++ model_test.py
@@ -15,5 +15,5 @@ pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
 
 output = pipe("<s>[INST] Conduct a RAS simulation for the turbulent flow over a backward-facing step using the pimpleFoam solver. Set the inlet velocity to 10 m/s and the outlet pressure to 0. The walls should be no-slip, and the front and back should be empty. Use the k-epsilon turbulence model for this simulation. The fluid is Newtonian with a kinematic viscosity of 1e-5 m²/s. The control settings are: endTime = 0.40 and writeInterval = 0.005.", do_sample=True)
 
-print(output[0]['text'])
+print(output[0]['generated_text'])
 
