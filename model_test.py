1a4ce557ed5693c86edd7d9ac6d5f39818ec529f
ramrow <peij.xu0@gmail.com>
2025-06-13 19:59:40 -0700

    added batch size

--- model_test.py
+++ model_test.py
@@ -1,6 +1,6 @@
 from transformers import pipeline
 
 model_name_or_path = "finalform/foam-nuTilda-llama-13B" #path/to/your/model/or/name/on/hub
-pipe = pipeline("text-generation", model=model_name_or_path,)
+pipe = pipeline("text-generation", model=model_name_or_path, batch_size=8)
 
 print(pipe("<s>[INST] Conduct a RAS simulation for the turbulent flow over a backward-facing step using the pimpleFoam solver. Set the inlet velocity to 10 m/s and the outlet pressure to 0. The walls should be no-slip, and the front and back should be empty. Use the k-epsilon turbulence model for this simulation. The fluid is Newtonian with a kinematic viscosity of 1e-5 m²/s. The control settings are: endTime = 0.40 and writeInterval = 0.005.")[0]["generated_text"])
