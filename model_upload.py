0bad2484fd9457567bdcb9030d35eeb45577944e
ramrow <peij.xu0@gmail.com>
2025-06-14 18:57:59 -0700

    update uploading

--- model_upload.py
+++ model_upload.py
@@ -5,6 +5,6 @@ token = input()
 api = HfApi(token=os.getenv(token))
 api.upload_folder(
     folder_path="./llama-foam",
-    repo_id="finalform/foam-nuTilda-llama-13B",
+    repo_id="finalform/foam-nuTilda-llama-2-13B-test",
     repo_type="model",
 )
