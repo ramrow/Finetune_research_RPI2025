1157b5326793ff046ae30ca54ee880a87ae314fe
ramrow <peij.xu0@gmail.com>
2025-06-15 09:19:38 -0700

    minor changes

--- finetune_m.py
+++ finetune_m.py
@@ -2,7 +2,7 @@ import os
 import torch
 from datasets import load_dataset
 from transformers import (
-    AutoModelForSeq2SeqLM,
+    AutoModelForCausalLM,
     AutoTokenizer,
     BitsAndBytesConfig,
     TrainingArguments,
@@ -39,7 +39,7 @@ ds = (load_dataset("finalform/processed_foam", split="train")).map(format_data)
 model="NousResearch/Llama-2-13b-hf"
 new_model = "llama-foam"
 
-md = AutoModelForSeq2SeqLM.from_pretrained(
+md = AutoModelForCausalLM.from_pretrained(
     model,
     quantization_config=quant_config,
     device_map={"": 0}
--- model_test.py
+++ model_test.py
@@ -1,9 +1,9 @@
-from transformers import pipeline, AutoModelForSeq2SeqLM, AutoTokenizer
+from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
 import torch
 
 model_name_or_path = "finalform/foam-nuTilda-sft-llama2-13B"
 
-model = AutoModelForSeq2SeqLM.from_pretrained(
+model = AutoModelForCausalLM.from_pretrained(
     model_name_or_path,
     torch_dtype=torch.float16,
     device_map="auto"  # Automatically place model on available GPUs
@@ -11,9 +11,9 @@ model = AutoModelForSeq2SeqLM.from_pretrained(
 
 tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
 
-pipe = pipeline("text2text-generation", model=model, tokenizer=tokenizer)
+pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
 
-output = pipe("<s>[INST] Conduct a laminar flow simulation around a cylinder using icoFoam. Apply an inlet velocity boundary condition, maintain a fixed zero pressure at the outlet, enforce a no-slip condition on the cylinder surface, and set zero-gradient conditions on the sides. Utilize a Newtonian fluid model with a kinematic viscosity of nu = 0.01 m^2/s. Initially, the velocity field inside the domain is (0.01, 0, 0) m/s, while the inlet velocity is (1, 0, 0) m/s. Control settings specify an endTime of 500 and a writeInterval of 40.",) # do_sample=True)
+output = pipe("<s>[INST] Conduct a laminar flow simulation around a cylinder using icoFoam. Apply an inlet velocity boundary condition, maintain a fixed zero pressure at the outlet, enforce a no-slip condition on the cylinder surface, and set zero-gradient conditions on the sides. Utilize a Newtonian fluid model with a kinematic viscosity of nu = 0.01 m^2/s. Initially, the velocity field inside the domain is (0.01, 0, 0) m/s, while the inlet velocity is (1, 0, 0) m/s. Control settings specify an endTime of 500 and a writeInterval of 40.", max_new_token= 512, do_sample=False)
 
 print(output[0])
 
