7afd03fadea7c172ce52113155202f73f66a224f
ramrow <peij.xu0@gmail.com>
2025-06-15 11:41:32 -0700

    minor changes

--- model_test.py
+++ model_test.py
@@ -13,18 +13,7 @@ tk = AutoTokenizer.from_pretrained(model_name_or_path)
 
 text = "<s>[INST] Conduct a laminar flow simulation around a cylinder using icoFoam. Apply an inlet velocity boundary condition, maintain a fixed zero pressure at the outlet, enforce a no-slip condition on the cylinder surface, and set zero-gradient conditions on the sides. Utilize a Newtonian fluid model with a kinematic viscosity of nu = 0.01 m^2/s. Initially, the velocity field inside the domain is (0.01, 0, 0) m/s, while the inlet velocity is (1, 0, 0) m/s. Control settings specify an endTime of 500 and a writeInterval of 40."
 
-
-# input_ids = tokenizer("<s>[INST] Conduct a laminar flow simulation around a cylinder using icoFoam. Apply an inlet velocity boundary condition, maintain a fixed zero pressure at the outlet, enforce a no-slip condition on the cylinder surface, and set zero-gradient conditions on the sides. Utilize a Newtonian fluid model with a kinematic viscosity of nu = 0.01 m^2/s. Initially, the velocity field inside the domain is (0.01, 0, 0) m/s, while the inlet velocity is (1, 0, 0) m/s. Control settings specify an endTime of 500 and a writeInterval of 40.", return_tensors="pt").to("cuda")
-
-# output = model.generate(**input_ids, cache_implementation="static")
-# print(output)
-# print(tokenizer.decode(output[0], skip_special_tokens=True))
-
-
 pipe = pipeline(task="text2text-generation", model=md, tokenizer=tk)
 output = pipe(text)
 print(output)
-# output = pipe(text="<s>[INST] Conduct a laminar flow simulation around a cylinder using icoFoam. Apply an inlet velocity boundary condition, maintain a fixed zero pressure at the outlet, enforce a no-slip condition on the cylinder surface, and set zero-gradient conditions on the sides. Utilize a Newtonian fluid model with a kinematic viscosity of nu = 0.01 m^2/s. Initially, the velocity field inside the domain is (0.01, 0, 0) m/s, while the inlet velocity is (1, 0, 0) m/s. Control settings specify an endTime of 500 and a writeInterval of 40.",max_new_tokens=512)
-
-# print(output)
 
