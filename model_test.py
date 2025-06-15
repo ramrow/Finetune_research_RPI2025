fe8acbcc3b9a967abe0191f2e6e9f874d8e933dc
ramrow <peij.xu0@gmail.com>
2025-06-15 10:41:42 -0700

    minor changes

--- model_test.py
+++ model_test.py
@@ -22,7 +22,7 @@ text = "<s>[INST] Conduct a laminar flow simulation around a cylinder using icoF
 
 
 pipe = pipeline(task="text-generation", model=md, tokenizer=tk)
-output = pipe(text,)
+output = pipe(input_ids=tk(text).to('cuda'),)
 print(output)
 # output = pipe(text="<s>[INST] Conduct a laminar flow simulation around a cylinder using icoFoam. Apply an inlet velocity boundary condition, maintain a fixed zero pressure at the outlet, enforce a no-slip condition on the cylinder surface, and set zero-gradient conditions on the sides. Utilize a Newtonian fluid model with a kinematic viscosity of nu = 0.01 m^2/s. Initially, the velocity field inside the domain is (0.01, 0, 0) m/s, while the inlet velocity is (1, 0, 0) m/s. Control settings specify an endTime of 500 and a writeInterval of 40.",max_new_tokens=512)
 
