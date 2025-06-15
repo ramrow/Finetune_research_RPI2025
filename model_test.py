88049c0b120dceb44bf30517c17ef5532b127a99
ramrow <peij.xu0@gmail.com>
2025-06-15 10:29:01 -0700

    minor changes

--- model_test.py
+++ model_test.py
@@ -3,13 +3,16 @@ import torch
 
 model_name_or_path = "finalform/foam-nuTilda-sft-llama2-13B"
 
-model = AutoModelForCausalLM.from_pretrained(
+md = AutoModelForCausalLM.from_pretrained(
     model_name_or_path,
     torch_dtype=torch.float16,
     device_map="auto"  # Automatically place model on available GPUs
 )
 
-tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+tk = AutoTokenizer.from_pretrained(model_name_or_path)
+
+text = "<s>[INST] Conduct a laminar flow simulation around a cylinder using icoFoam. Apply an inlet velocity boundary condition, maintain a fixed zero pressure at the outlet, enforce a no-slip condition on the cylinder surface, and set zero-gradient conditions on the sides. Utilize a Newtonian fluid model with a kinematic viscosity of nu = 0.01 m^2/s. Initially, the velocity field inside the domain is (0.01, 0, 0) m/s, while the inlet velocity is (1, 0, 0) m/s. Control settings specify an endTime of 500 and a writeInterval of 40."
+
 
 # input_ids = tokenizer("<s>[INST] Conduct a laminar flow simulation around a cylinder using icoFoam. Apply an inlet velocity boundary condition, maintain a fixed zero pressure at the outlet, enforce a no-slip condition on the cylinder surface, and set zero-gradient conditions on the sides. Utilize a Newtonian fluid model with a kinematic viscosity of nu = 0.01 m^2/s. Initially, the velocity field inside the domain is (0.01, 0, 0) m/s, while the inlet velocity is (1, 0, 0) m/s. Control settings specify an endTime of 500 and a writeInterval of 40.", return_tensors="pt").to("cuda")
 
@@ -18,9 +21,10 @@ tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
 # print(tokenizer.decode(output[0], skip_special_tokens=True))
 
 
-pipe = pipeline( model=model, tokenizer=tokenizer)
-
-output = pipe(input_ids="<s>[INST] Conduct a laminar flow simulation around a cylinder using icoFoam. Apply an inlet velocity boundary condition, maintain a fixed zero pressure at the outlet, enforce a no-slip condition on the cylinder surface, and set zero-gradient conditions on the sides. Utilize a Newtonian fluid model with a kinematic viscosity of nu = 0.01 m^2/s. Initially, the velocity field inside the domain is (0.01, 0, 0) m/s, while the inlet velocity is (1, 0, 0) m/s. Control settings specify an endTime of 500 and a writeInterval of 40.",max_new_tokens=512)
-
+pipe = pipeline(task="text-generation", model=md, tokenizer=tk)
+output = pipe(text,)
 print(output)
+# output = pipe(text="<s>[INST] Conduct a laminar flow simulation around a cylinder using icoFoam. Apply an inlet velocity boundary condition, maintain a fixed zero pressure at the outlet, enforce a no-slip condition on the cylinder surface, and set zero-gradient conditions on the sides. Utilize a Newtonian fluid model with a kinematic viscosity of nu = 0.01 m^2/s. Initially, the velocity field inside the domain is (0.01, 0, 0) m/s, while the inlet velocity is (1, 0, 0) m/s. Control settings specify an endTime of 500 and a writeInterval of 40.",max_new_tokens=512)
+
+# print(output)
 
