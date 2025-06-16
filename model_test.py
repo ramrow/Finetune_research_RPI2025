c1b40e08750a78bd9030a49a0c047e3f5db6412f
ramrow <peij.xu0@gmail.com>
2025-06-15 19:10:22 -0700

    change paths

--- model_test.py
+++ model_test.py
@@ -14,7 +14,14 @@ tk = AutoTokenizer.from_pretrained(model_name_or_path)
 
 text = "<s>[INST] Conduct a laminar flow simulation around a cylinder using icoFoam. Apply an inlet velocity boundary condition, maintain a fixed zero pressure at the outlet, enforce a no-slip condition on the cylinder surface, and set zero-gradient conditions on the sides. Utilize a Newtonian fluid model with a kinematic viscosity of nu = 0.01 m^2/s. Initially, the velocity field inside the domain is (0.01, 0, 0) m/s, while the inlet velocity is (1, 0, 0) m/s. Control settings specify an endTime of 500 and a writeInterval of 40."
 
-pipe = pipeline(task="text2text-generation", model=md, tokenizer=tk)
-output = pipe(text)
-print(output)
+
+
+pipe = pipeline(task="text-generation", model=md, tokenizer=tk, device_map="auto")
+
+messages = [
+    {"role": "user", "content": text},
+]
+
+output = pipe(messages, max_new_tokens=512)
+print(output[0])
 
