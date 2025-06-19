ffedc7e48420574dd4875d68408ff78769573a54
ramrow <peij.xu0@gmail.com>
2025-06-18 18:34:43 -0700

    foam llama test

--- model_test.py
+++ model_test.py
@@ -23,9 +23,7 @@ text = "Conduct a RAS simulation for the turbulent flow over a backward-facing s
 pipe = pipeline(task="text-generation", model=md, tokenizer=tk, device_map={"":0})
 
 messages = [
-    {"role": "user", "content": text},
-    {"role": "system", "content": prompt}
-
+    {"role": "system", "content": prompt + " " + text},
 ]
 
 output = pipe(messages)
