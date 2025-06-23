25a9ab1029ac5c56c52b0e8cf344ee1356fcca1d
ramrow <peij.xu0@gmail.com>
2025-06-22 19:51:49 -0700

    small updates

--- finetune_real.py
+++ finetune_real.py
@@ -30,7 +30,7 @@ def apply_chat_template(example):
     return {"text": prompt}
 
 def tokenize_data(example):
-    tokens = tokenizer(example['text'], padding="max_length", max_length=2056)
+    tokens = tokenizer(example['text'], padding="max_length", max_length=4096)
     tokens['labels'] = [
         -100 if token == tokenizer.pad_token_id else token for token in tokens['input_ids']
     ]
@@ -71,7 +71,7 @@ peft_params = LoraConfig(
 training_args = SFTConfig(
     output_dir="./llamaResultsFormatted",
     # resume_from_checkpoint="./llamaResultsFormatted/checkpoint-",
-    num_train_epochs=2,
+    num_train_epochs=1,
     per_device_train_batch_size=2,
     per_device_eval_batch_size=2,
     # per_device_train_batch_size=1,
@@ -83,7 +83,7 @@ training_args = SFTConfig(
     learning_rate=2e-4,
     weight_decay=0.001,
     fp16=False,
-    bf16=False,
+    bf16=True,
     max_grad_norm=0.3,
     max_steps=-1,
     warmup_ratio=0.03,
