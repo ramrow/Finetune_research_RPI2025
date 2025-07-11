5ab10e4f940acd8a6efee228003debe2991e8bce
ramrow <peij.xu0@gmail.com>
2025-07-11 13:54:37 -0700

    changed gradient step

--- finetune_qwen.py
+++ finetune_qwen.py
@@ -86,8 +86,8 @@ training_args = SFTConfig(
     # resume_from_checkpoint="./qwen_results/checkpoint-",
     # compute loss every few steps 1.5k/step
     num_train_epochs=1,
-    per_device_train_batch_size=3,
-    per_device_eval_batch_size=3,
+    per_device_train_batch_size=2,
+    per_device_eval_batch_size=2,
     gradient_accumulation_steps=8,
     optim="paged_adamw_32bit",
     save_steps=250,
