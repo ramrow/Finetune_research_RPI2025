c6b77d6218930194b181afaa2951b76e6395adcd
ramrow <peij.xu0@gmail.com>
2025-07-13 16:55:02 -0700

    error correction

--- torchtune_qwen.py
+++ torchtune_qwen.py
@@ -79,8 +79,8 @@ class torch_prep():
 
         self.output_dir="./qwen_results"
         self.num_train_epochs=1
-        self.per_device_train_batch_size=2
-        self.per_device_eval_batch_size=2
+        self.per_device_train_batch_size=1
+        self.per_device_eval_batch_size=1
         self.gradient_accumulation_steps=6
         self.optim="paged_adamw_32bit"
         self.save_steps=250
