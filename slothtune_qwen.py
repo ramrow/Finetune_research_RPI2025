3ace05296a73697b8aef4873a8ec420f68019e1a
ramrow <peij.xu0@gmail.com>
2025-07-16 11:18:52 -0700

    update number of workers

--- slothtune_qwen.py
+++ slothtune_qwen.py
@@ -33,7 +33,7 @@ class unsloth_qwen():
     def finetune(self):
         training_args = SFTConfig(
             output_dir="./qwen_results",
-            resume_from_checkpoint="./qwen_results/checkpoint-",
+            resume_from_checkpoint="./qwen_results/checkpoint-3000",
             num_train_epochs=1,
             per_device_train_batch_size=2,
             per_device_eval_batch_size=2,
