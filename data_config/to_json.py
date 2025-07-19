3c018aab273379e5584991c74928d82d67383a4b
ramrow <peij.xu0@gmail.com>
2025-07-19 16:06:40 -0700

    switched back to json

--- data_config/to_json.py
+++ data_config/to_json.py
@@ -1,7 +1,15 @@
 import pandas as pd
+import csv
+import json
 
 # Read CSV file
-df = pd.read_csv('train.csv')
+# df = pd.read_csv('data/train.csv')
 
-# DataFrame to JSON
-df.to_json('train.json', orient='records', lines=True, indent=2)
+# # DataFrame to JSON
+# df.to_json('data/train.json', orient='records', lines=True)
+
+with open('data/train.csv', mode='r', newline='', encoding='utf-8') as csvfile:
+    data = list(csv.DictReader(csvfile))
+
+with open('data/train.json', mode='w', encoding='utf-8') as jsonfile:
+    json.dump(data, jsonfile, indent=4)
