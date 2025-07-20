f68bd289f5bd5bd0475b12ee4aa41bbd2fd5531d
ramrow <peij.xu0@gmail.com>
2025-07-19 19:16:07 -0700

    new model

--- model_test.py
+++ model_test.py
@@ -13,7 +13,7 @@ model_name_or_path = "finalform/factoryqwen-8B"
 
 md = AutoModelForCausalLM.from_pretrained(
     model_name_or_path,
-    torch_dtype=torch.float16,
+    torch_dtype="auto",
     device_map="auto",
     trust_remote_code=True
 )
@@ -32,20 +32,38 @@ messages = [
 text = tk.apply_chat_template(
     messages,
     tokenize=False,
-    add_generation_prompt=True
+    add_generation_prompt=True,
+    enable_thinking=True
 )
 
 model_inputs = tk([text], return_tensors="pt").to(md.device)
+# generated_ids = md.generate(
+#     **model_inputs,
+#     # max_new_tokens=1028
+# )
 generated_ids = md.generate(
     **model_inputs,
-    max_new_tokens=1028
 )
-generated_ids = [
-    output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
-]
+output_ids = generated_ids[0][len(model_inputs.input_ids[0]):].tolist() 
 
-response = tk.batch_decode(generated_ids, skip_special_tokens=True)[0]
-print(response)
+# parsing thinking content
+try:
+    # rindex finding 151668 (</think>)
+    index = len(output_ids) - output_ids[::-1].index(151668)
+except ValueError:
+    index = 0
+
+thinking_content = tk.decode(output_ids[:index], skip_special_tokens=True).strip("\n")
+content = tk.decode(output_ids[index:], skip_special_tokens=True).strip("\n")
+
+print("thinking content:", thinking_content)
+print("content:", content)
+# generated_ids = [
+#     output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+# ]
+
+# response = tk.batch_decode(generated_ids, skip_special_tokens=True)[0]
+# print(response)
 
 # output = pipe(messages, )
 # result = output[0]['generated_text'][-1]['content']
