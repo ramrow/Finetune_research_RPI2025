3c795dfbcb4628a094a050559796c4f274aadb8e
ramrow <peij.xu0@gmail.com>
2025-07-21 10:16:52 -0700

    model evaluation with transformers

---
+++ model_eval.py
@@ -0,0 +1,103 @@
+import os
+import torch
+from datasets import load_dataset
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    AutoModelForVision2Seq ,
+)
+from peft import LoraConfig, get_peft_model
+from trl import SFTTrainer, SFTConfig
+
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+
+quant_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16,
+    bnb_4bit_use_double_quant=True,
+)
+
+def format_data_helper(self, example):
+        messages = [
+            {"role": "system", "content": example["system_prompt"]},
+            {"role": "user", "content": example['usr_prompt']},
+            {"role": "assistant", "content": example["code_content"]}
+        ]
+        prompt = self.tk.apply_chat_template(
+            messages, tokenize=False, add_generation_prompt=True
+        )
+        tokens = self.tk(prompt, padding="max_length", max_length=1028, truncation=True)
+        tokens['labels'] = [
+            -100 if token == self.tk.pad_token_id else token for token in tokens['input_ids']
+        ]
+        return tokens
+
+
+ds = (load_dataset("finalform/split_foam",))
+model="finalform/factoryqwen-8B"
+
+md = AutoModelForVision2Seq.from_pretrained(
+    model,
+    quantization_config=quant_config,
+    device_map="auto",
+    trust_remote_code=True,
+    torch_dtype=torch.bfloat16,
+)
+
+md.config.use_cache = False
+md.config.pretraining_tp = 1
+
+tokenizer = AutoTokenizer.from_pretrained(model, trust_remote_code=True)
+tokenizer.return_tensors = "pt"
+tokenizer.pad_token = tokenizer.eos_token
+# print(tokenizer.pad_token, tokenizer.eos_token)
+# tokenizer.pad_token = '<|endoftext|>'
+# tokenizer.eos_token = '<|endoftext|>'
+tokenizer.padding_side = "right"
+tsd = ds['test'].map(format_data_helper).remove_columns(["system_prompt", "usr_prompt", 
+                                                                                "folder_name", "file_name", "case_path", "description", "code_content"])
+peft_params = LoraConfig(
+    lora_alpha=16,
+    lora_dropout=0.1,
+    r=32, #change rank
+    bias="none",
+    task_type="CAUSAL_LM",
+    target_modules="all-linear"
+)
+
+training_args = SFTConfig(
+    output_dir="./qwen_results",
+    # resume_from_checkpoint="./qwen_results/checkpoint-",
+    # compute loss every few steps 1.5k/step
+    num_train_epochs=1,
+    per_device_train_batch_size=2,
+    per_device_eval_batch_size=2,
+    gradient_accumulation_steps=4, #2
+    optim="paged_adamw_32bit", #paged_adamw_32bit
+    save_steps=250,
+    logging_steps=25,
+    learning_rate=3e-4,
+    weight_decay=0.001,
+    fp16=False,
+    bf16=True,
+    max_grad_norm=0.3,
+    max_steps=-1,
+    warmup_ratio=0.03,
+    group_by_length=True,
+    lr_scheduler_type="constant",
+    report_to="tensorboard",
+    packing=False,
+)
+
+peft_md = get_peft_model(md, peft_params)
+
+trainer = SFTTrainer(
+    model=peft_md,
+    eval_dataset=tsd,
+    args=training_args,
+    processing_class=tokenizer,
+)
+
+trainer.evaluate()
