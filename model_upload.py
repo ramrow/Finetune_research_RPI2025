c336cec5609d2583564d3341a1d0fa9f1941ae90
ramrow <peij.xu0@gmail.com>
2025-07-30 12:34:10 -0700

    new model

--- model_upload.py
+++ model_upload.py
@@ -5,6 +5,6 @@ token = input()
 api = HfApi(token=os.getenv(token))
 api.upload_folder(
     folder_path="./factory_qwen_results",
-    repo_id="finalform/testfoam",
+    repo_id="finalform/testfoam-2.5coder",
     repo_type="model",
 )
