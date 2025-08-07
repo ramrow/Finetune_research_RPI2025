798ce277ba972bcdefe46a8d0dbf305a2fde737c
ramrow <peij.xu0@gmail.com>
2025-08-06 19:38:11 -0700

    mistral 0.3 model

--- model_test.py
+++ model_test.py
@@ -34,7 +34,7 @@ model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
 
 generated_ids = model.generate(
     **model_inputs,
-    max_new_tokens=1028
+    max_new_tokens=512
 )
 generated_ids = [
     output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
