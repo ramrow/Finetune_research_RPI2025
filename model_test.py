c3ab9bf240b9a70f26e00c8db4f18332401e5f96
ramrow <peij.xu0@gmail.com>
2025-08-09 08:11:06 -0700

    qwen coder

--- model_test.py
+++ model_test.py
@@ -11,7 +11,7 @@ import os
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 
-model_name = "finalform/foamMistral0.3-7B-Instruct-trl"
+model_name = "finalform/foamQwen2.5-7B-Coder-trl"
 
 # load the tokenizer and the model
 tokenizer = AutoTokenizer.from_pretrained(model_name)
