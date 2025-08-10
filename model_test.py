2b700b0ddb2b7406b8883a24c73c7a09e6cd2041
ramrow <peij.xu0@gmail.com>
2025-08-10 12:54:49 -0700

    llama thinking content

--- model_test.py
+++ model_test.py
@@ -11,7 +11,7 @@ import os
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 
-model_name = "finalform/foamQwen2.5-7B-Coder-trl"
+model_name = "finalform/foamLlama3.1-8B-Instruct-trl"
 
 # load the tokenizer and the model
 tokenizer = AutoTokenizer.from_pretrained(model_name)
