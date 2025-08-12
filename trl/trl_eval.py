7d9a20c0f35e03c3f86b450f1173c7739daa8489
ramrow <peij.xu0@gmail.com>
2025-08-12 10:30:30 -0700

    combat memory error

--- trl/trl_eval.py
+++ trl/trl_eval.py
@@ -80,9 +80,9 @@ training_args = SFTConfig(
     output_dir="foamqwen",
     # resume_from_checkpoint="./qwen_results/checkpoint-",
     # compute loss every few steps 1.5k/step
-    num_train_epochs=3,
-    per_device_train_batch_size=2,
-    per_device_eval_batch_size=2,
+    num_train_epochs=1,
+    per_device_train_batch_size=1,
+    per_device_eval_batch_size=1,
     gradient_accumulation_steps=4, #2
     # gradient_checkpointing=True,
     optim="paged_adamw_32bit", #paged_adamw_32bit
