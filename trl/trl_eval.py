56ea34b613925087610cd689bd8b41a63e996a9c
ramrow <peij.xu0@gmail.com>
2025-08-13 15:36:39 -0700

    smaller model

--- trl/trl_eval.py
+++ trl/trl_eval.py
@@ -41,7 +41,7 @@ def tokenize_data(example):
 
 
 ds = (load_dataset("LeoYML/FoamGPT",)).shuffle()
-model="Qwen/Qwen3-Coder-30B-A3B-Instruct"
+model="Qwen/Qwen2.5-Coder-14B-Instruct"
 new_model = "foamqwen"
 
 md = AutoModelForCausalLM.from_pretrained(
@@ -80,11 +80,10 @@ training_args = SFTConfig(
     output_dir="foamqwen",
     # resume_from_checkpoint="./qwen_results/checkpoint-",
     # compute loss every few steps 1.5k/step
-    num_train_epochs=1,
-    per_device_train_batch_size=1,
-    per_device_eval_batch_size=1,
+    num_train_epochs=6,
+    per_device_train_batch_size=2,
+    per_device_eval_batch_size=2,
     gradient_accumulation_steps=4, #2
-    gradient_checkpointing=True,
     optim="paged_adamw_32bit", #paged_adamw_32bit
     # save_steps=750,
     logging_steps=25,
--- trl/trl_train.py
+++ trl/trl_train.py
@@ -43,7 +43,7 @@ def tokenize_data(example):
 
 
 ds = (load_dataset("LeoYML/FoamGPT",)).shuffle()
-model="Qwen/Qwen3-Coder-30B-A3B-Instruct"
+model="Qwen/Qwen2.5-Coder-14B-Instruct"
 new_model = "foamqwen"
 
 
@@ -86,9 +86,9 @@ training_args = SFTConfig(
     output_dir="foamqwen",
     # resume_from_checkpoint="./qwen_results/checkpoint-",
     # compute loss every few steps 1.5k/step
-    num_train_epochs=1,
-    per_device_train_batch_size=1,
-    per_device_eval_batch_size=1,
+    num_train_epochs=6,
+    per_device_train_batch_size=2,
+    per_device_eval_batch_size=2,
     gradient_accumulation_steps=4, 
     # gradient_checkpointing=True,
     optim="paged_adamw_32bit",
