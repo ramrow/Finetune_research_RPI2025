fe861de64974cb72a700808943a06b82000faf2e
ramrow <peij.xu0@gmail.com>
2025-08-14 12:51:40 -0700

    dataset comparison

--- model_upload.py
+++ model_upload.py
@@ -4,7 +4,7 @@ import os
 token = input()
 api = HfApi(token=os.getenv(token))
 api.upload_folder(
-    folder_path="./qwen-foam",
-    repo_id="finalform/foamqwen2.5",
-    repo_type="model",
+    folder_path="bernardFOAM.csv",
+    repo_id="finalform/bernardFOAM",
+    repo_type="dataset",
 )
