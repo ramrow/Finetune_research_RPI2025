cbcc74a7f833168b4e79c4f3ad264353abc7e06b
ramrow <peij.xu0@gmail.com>
2025-08-16 17:58:46 -0700

    qwen 14B instruct inference

--- model_test.py
+++ model_test.py
@@ -41,7 +41,7 @@ model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
 # conduct text completion
 generated_ids = model.generate(
     **model_inputs,
-    max_new_tokens=1024,
+    max_new_tokens=56864,
     temperature=1,
 )
 output_ids = generated_ids[0][len(model_inputs.input_ids[0]):].tolist() 
