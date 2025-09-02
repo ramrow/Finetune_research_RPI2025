3b5da42496374863494501ca4bf132f305731a02
ramrow <peij.xu0@gmail.com>
2025-09-02 14:27:19 -0400

    counterflowflame test

--- test/model_test.py
+++ test/model_test.py
@@ -44,7 +44,7 @@ model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
 # conduct text completion
 generated_ids = model.generate(
     **model_inputs,
-    max_new_tokens=327680,
+    max_new_tokens=32768//4,
     temperature=1,
 )
 output_ids = generated_ids[0][len(model_inputs.input_ids[0]):].tolist() 
