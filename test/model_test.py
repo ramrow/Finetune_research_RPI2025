3984a7d13fdfde546cf26e262b99e3fd49f18237
ramrow <peij.xu0@gmail.com>
2025-09-19 23:23:46 -0400

    new model

--- test/model_test.py
+++ test/model_test.py
@@ -11,7 +11,7 @@ import os
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 
-model_name = "finalform/foamQwen2.5-7B-Instruct-hope"
+model_name = "finalform/foamQwen2.5-7B-Instruct-trl"
 
 # load the tokenizer and the model
 tokenizer = AutoTokenizer.from_pretrained(model_name)
