5b50940b829c304ab707e16df8197af5635f3022
ramrow <peij.xu0@gmail.com>
2025-11-07 20:57:14 -0500

    upload method

--- model_upload.py
+++ model_upload.py
@@ -2,7 +2,7 @@ from huggingface_hub import HfApi
 import os
 
 token = input()
-api = HfApi(token=os.getenv(token))
+api = HfApi(token=os.getenv(token), timeout=120.0)
 api.upload_folder(
     folder_path="foamqwen/checkpoint-312",
     repo_id="finalform/foamQwen2.5-7B-Instruct-similar",
