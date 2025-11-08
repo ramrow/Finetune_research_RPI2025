741cbba8d8c5ca886e7d37a62af090e9502fe4ca
ramrow <peij.xu0@gmail.com>
2025-11-07 21:12:19 -0500

    one file at a time

--- model_upload.py
+++ model_upload.py
@@ -4,7 +4,7 @@ import os
 token = input()
 api = HfApi(token=os.getenv(token))
 api.upload_large_folder(
-    folder_path="foamqwen/checkpoint-312",
+    folder_path="foamqwen/checkpoint-312/adapter_config.json",
     repo_id="finalform/foamQwen2.5-7B-Instruct-similar",
     repo_type="model",
 )
