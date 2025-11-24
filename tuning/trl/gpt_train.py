05a53cda300cf3acd747f65854798b228b735059
ramrow <peij.xu0@gmail.com>
2025-11-23 20:38:21 -0500

    gpt oss training

--- tuning/trl/gpt_train.py
+++ tuning/trl/gpt_train.py
@@ -17,27 +17,38 @@ model_kwargs = dict(
     device_map="auto",
 )
 
-def apply_chat_template(example):
-    messages = [
-        {"role": "system", "content": example["system_prompt"]},
-        {"role": "user", "content": example['user_prompt']},
-        {"role": "assistant", "content": example["file_content"]}
-    ]
-    prompt = tokenizer.apply_chat_template(
-        messages, tokenize=False, add_generation_prompt=True
-    )
-    return {"text": prompt}
+# def apply_chat_template(example):
+#     messages = [
+#         {"role": "system", "content": example["system_prompt"]},
+#         {"role": "user", "content": example['user_prompt']},
+#         {"role": "assistant", "content": example["file_content"]}
+#     ]
+#     prompt = tokenizer.apply_chat_template(
+#         messages, tokenize=False, add_generation_prompt=True
+#     )
+#     return {"text": prompt}
 
-def tokenize_data(example):
-    tokens = tokenizer(example['text'], padding="longest",)
-    # tokens = tokenizer(example['text'], padding="max_length", max_length=1028, truncation=True)
-    tokens['labels'] = [
-        -100 if token == tokenizer.pad_token_id else token for token in tokens['input_ids']
-    ]
-    return tokens
+# def tokenize_data(example):
+#     tokens = tokenizer(example['text'], padding="longest",)
+#     # tokens = tokenizer(example['text'], padding="max_length", max_length=1028, truncation=True)
+#     tokens['labels'] = [
+#         -100 if token == tokenizer.pad_token_id else token for token in tokens['input_ids']
+#     ]
+#     return tokens
+def split_prompt_completion(example):
+    return {
+        "prompt": [
+            {"role": "system", "content": example["system_prompt"]},
+            {"role": "user",   "content": example["user_prompt"]},
+        ],
+        "completion": [
+            {"role": "assistant", "content": example["file_content"]},
+        ],
+    }
+
 
 model = AutoModelForCausalLM.from_pretrained("openai/gpt-oss-20b", **model_kwargs)
-ds = (load_dataset("LeoYML/FoamGPT",)).shuffle()
+ds = (load_dataset("finalform/foamGPT-old",)).shuffle()
 new_model = "foamGPT"
 
 tokenizer = AutoTokenizer.from_pretrained("openai/gpt-oss-20b")
@@ -45,12 +56,18 @@ tokenizer = AutoTokenizer.from_pretrained("openai/gpt-oss-20b")
 tokenizer.return_tensors = "pt"
 tokenizer.pad_token = tokenizer.eos_token
 tokenizer.padding_side = "right"
-train_ds = ds['train'].map(apply_chat_template)
-test_ds = ds['test'].map(apply_chat_template)
-tokenized_train_ds = train_ds.map(tokenize_data)
-tokenized_test_ds = test_ds.map(tokenize_data)
-tokenized_train_ds = tokenized_train_ds.remove_columns(["text", "system_prompt", "user_prompt", "folder_name", "file_name", "case_name", "case_domain", "user_requirement", "file_content", "case_category", "case_solver"])
-tokenized_test_ds = tokenized_test_ds.remove_columns(["text", "system_prompt", "user_prompt", "folder_name", "file_name", "case_name", "case_domain", "user_requirement", "file_content", "case_category", "case_solver"])
+# Apply template
+ds = ds.map(
+    split_prompt_completion,
+    remove_columns=ds["train"].column_names,
+    num_proc=8,
+)
+# train_ds = ds['train'].map(apply_chat_template)
+# test_ds = ds['test'].map(apply_chat_template)
+# tokenized_train_ds = train_ds.map(tokenize_data)
+# tokenized_test_ds = test_ds.map(tokenize_data)
+# tokenized_train_ds = tokenized_train_ds.remove_columns(["text", "system_prompt", "user_prompt", "folder_name", "file_name", "case_name", "case_domain", "user_requirement", "file_content", "case_category", "case_solver"])
+# tokenized_test_ds = tokenized_test_ds.remove_columns(["text", "system_prompt", "user_prompt", "folder_name", "file_name", "case_name", "case_domain", "user_requirement", "file_content", "case_category", "case_solver"])
 
 peft_config = LoraConfig(
     r=8,
@@ -79,8 +96,8 @@ training_args = SFTConfig(
     optim="paged_adamw_32bit",
     # save_steps=750,
     logging_steps=25,
-    learning_rate=3e-4,
-    weight_decay=0.001,
+    learning_rate=5.11e-4,
+    weight_decay=0.03,
     fp16=False,
     bf16=True,
     max_grad_norm=0.3,
@@ -92,12 +109,15 @@ training_args = SFTConfig(
     packing=False,
     eval_strategy="epoch",
     save_strategy="epoch",
+    dataset_text_field="text",
+    assistant_only_loss=True,
+    completion_only_loss=True
 )
 
 trainer = SFTTrainer(
     model=peft_md,
-    train_dataset=tokenized_train_ds,
-    eval_dataset=tokenized_test_ds,
+    train_dataset=ds['train'],
+    eval_dataset=ds['test'],
     args=training_args,
     processing_class=tokenizer,
 )
